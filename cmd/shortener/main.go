38acc43a105558b790254b9904363ac064e711b3
Sergey Markelov <markelo.v@ya.ru>
2026-02-01 15:02:18 +0000

    Инкремент 23

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/MarkelovSergey/url-shorter/internal/app"
 	"github.com/MarkelovSergey/url-shorter/internal/config"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	printBuildInfo()
 	cfg := config.ParseFlags()
 	app := app.New(cfg)
 
 	if err := app.Run(); err != nil {
 		log.Fatal("Application error:", err)
 	}
+}
+
+func printBuildInfo() {
+	version := buildVersion
+	if version == "" {
+		version = "N/A"
+	}
+	date := buildDate
+	if date == "" {
+		date = "N/A"
+	}
+	commit := buildCommit
+	if commit == "" {
+		commit = "N/A"
+	}
+
+	fmt.Printf("Build version: %s\n", version)
+	fmt.Printf("Build date: %s\n", date)
+	fmt.Printf("Build commit: %s\n", commit)
 }
